81645e6783fff72795b3b270dbccc80a655c6742
Jani Halmetoja <jani.halmetoja@gmail.com>
2014-12-07 16:16:39 +0200

    Adding more data types

--- client/easy-search-array-test.js
+++ client/easy-search-array-test.js
@@ -3,35 +3,75 @@ function type(obj){
 }
 
 UI.body.helpers({
-  results: function () {
+  elasticSearchResults: function () {
     return Session.get("dataResults");  
   }
 });
 
-UI.body.events({
-  'click #search' : function() {
+UI.body.rendered = function() {
     EasySearch.search('test', "my-array-test", function (err, data) {
         if (err) console.log(err.error);
         Session.set("dataResults", data.results);
     });
-  }
-})
+}
 
 Template.result.helpers({
-  arrayTest: function () {     
+  test: function(expect, object) {
+    return (typeof object === expect) ? 'pass' : 'fail';
+  },
+  testArray: function(object) {
+    return (object instanceof Array) ? 'pass' : 'fail';
+  },
+
+
+  mongoArray: function() {
+    return CollectionIncludingArray.findOne({}) ? type(CollectionIncludingArray.findOne({}).myArray) : '';
+  },
+  mongoString: function() {
+    return CollectionIncludingArray.findOne({}) ? type(CollectionIncludingArray.findOne({}).myString) : '';
+  },
+  mongoInt: function() {
+    return CollectionIncludingArray.findOne({}) ? type(CollectionIncludingArray.findOne({}).myInt) : '';
+  },
+  mongoBoolean: function() {
+    return CollectionIncludingArray.findOne({}) ? type(CollectionIncludingArray.findOne({}).myBoolean) : '';
+  },
+  mongoObject: function() {
+    return CollectionIncludingArray.findOne({}) ? type(CollectionIncludingArray.findOne({}).myObject) : '';
+  },
+  mongoNull: function() {
+    return CollectionIncludingArray.findOne({}) ? type(CollectionIncludingArray.findOne({}).myNull) : '';
+  },
+  mongoUndefined: function() {
+    return CollectionIncludingArray.findOne({}) ? type(CollectionIncludingArray.findOne({}).myUndefined) : '';
+  },
+
+  esArray: function () {     
     console.log(this.myArray); 
     return type(this.myArray);
   },
-  stringTest: function () {      
+  esString: function () {      
     console.log(this.myString); 
     return type(this.myString);
   },
-  intTest: function () {      
+  esInt: function () {      
     console.log(this.myInt); 
     return type(this.myInt);
   },
-  booleanTest: function () {      
+  esBoolean: function () {      
     console.log(this.myBoolean); 
     return type(this.myBoolean);
+  },
+  esObject: function () {      
+    console.log(this.myObject); 
+    return type(this.myObject);
+  },
+  esNull: function () {      
+    console.log(this.myNull); 
+    return type(this.myNull);
+  },
+  esUndefined: function () {      
+    console.log(this.myUndefined); 
+    return type(this.myUndefined);
   }
 });
--- server/main.js
+++ server/main.js
@@ -1,4 +1,13 @@
 Meteor.startup(function() {
 	CollectionIncludingArray.remove({});
-	CollectionIncludingArray.insert({title: "my-array-test", myArray: ["123", "456", "789"], myString:"123", myInt:1, myBoolean:true });
+	CollectionIncludingArray.insert({
+		title: "my-array-test", 
+		myArray: ["123", "456", "789"], 
+		myString: "123", 
+		myInt: 1, 
+		myBoolean: true,
+		myObject: {firstName: "123", lastname: "456"},
+		myNull: null,
+		myUndefined: undefined
+	});
 });
